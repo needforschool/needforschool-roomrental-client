e1ab4e839dec651bd58428766107ebfe2a3ba330
Antoine Kingue <etorefr@gmail.com>
2022-08-28 15:30:57 +0200

    🎨 improve api service with get rooms and get rentals

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,24 @@
-import { Room } from "@typeDefs/index";
+import { Rental, Room } from "@typeDefs/main";
 import axios from "axios";
 
 export const endpoint = process.env.NEXT_PUBLIC_API_URL || "";
+
+export const getRooms = async (): Promise<undefined | Room[]> => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "get",
+    url: `${endpoint}/rooms`,
+  };
+
+  try {
+    const req = await axios(config);
+    return req.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const searchRooms = async (values: any): Promise<Room[] | undefined> => {
   const config = {
@@ -23,7 +40,10 @@ export const searchRooms = async (values: any): Promise<Room[] | undefined> => {
   }
 };
 
-export const createRental = async (room: Room, values: any): Promise<any> => {
+export const createRental = async (
+  room: Room,
+  values: any
+): Promise<undefined | Rental> => {
   const config = {
     headers: {
       "Content-Type": "application/json",
@@ -33,6 +53,47 @@ export const createRental = async (room: Room, values: any): Promise<any> => {
     data: {
       room,
       ...values,
+    },
+  };
+
+  try {
+    const req = await axios(config);
+    return req.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getRentals = async (): Promise<undefined | Rental[]> => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "get",
+    url: `${endpoint}/rentals`,
+  };
+
+  try {
+    const req = await axios(config);
+    return req.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// update rental with valid to true
+export const validateRental = async (
+  rental: Rental
+): Promise<undefined | Rental[]> => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "put",
+    url: `${endpoint}/rentals/${rental.id}`,
+    data: {
+      ...rental,
+      valid: true,
     },
   };
 
